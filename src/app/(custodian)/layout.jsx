51e5ea6a33ad920aee5cf8688187a6ffa1bbf6f6
cluab <cluab11222@gmail.com>
2023-06-16 17:44:55 +0300

    Add Layout Files

---
+++ src/app/(custodian)/layout.jsx
@@ -0,0 +1,13 @@
+import "@/styles/global.css";
+
+
+export default function AuthRootLayout({ children }) {
+    return (
+        <html lang="en">
+            <head />
+            <body>
+                    {children}   
+        </body>
+        </html>
+    )
+}
