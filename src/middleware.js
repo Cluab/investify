dc20c271afd249945aac1d0251915c662486a1d1
cluab <cluab11222@gmail.com>
2023-06-24 14:49:47 +0300

    Add Middleware

---
+++ src/middleware.js
@@ -0,0 +1,9 @@
+import { NextResponse } from 'next/server'
+
+export function middleware(req) {   
+  const url = req.nextUrl.clone()   
+  if (url.pathname === '/') {
+    url.pathname = '/home'
+    return NextResponse.redirect(url)   
+  } 
+}
